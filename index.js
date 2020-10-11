b74a6e32918ab683a709124e8e2cdd9f0c970251
Clement Plasse <clement.plasse.lyon@gmail.com>
2020-10-11 22:18:58 +0200

    first view to add match

--- index.js
+++ index.js
@@ -7,34 +7,47 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 (async() => {
-  router.get('/player', async function(req, res) {
+  router.get('/player', async (_, res) => {
     const players = await db.all('select * from player order by elo desc');
     res.send(players);
   })
 
-  router.get('/game', async function(req, res) {
+  router.get('/game', async (_, res) => {
     const games = await db.all('select * from game');
     res.send(games);
   })
 
-  router.post('/game', async function(req, res) {
-    const { player1Id, player2Id, sets, simulate } = req.body;
+  router.post('/game', async (req, res) => {
+    const { player1Id, player2Id, player1Scores, player2Scores, simulate } = req.body;
     const player1 = await db.get(`select * from player where player_id=${player1Id}`);
     const player2 = await db.get(`select * from player where player_id=${player2Id}`);
     let player1TotalScore = 0;
     let player2TotalScore = 0;
     let player1TotalVictory = 0;
     let player2TotalVictory = 0;
-    for (let set of sets) {
-      player1TotalScore += set.player1Score;
-      player2TotalScore += set.player2Score;
-      if (set.player1Score > set.player2Score) {
-        player1TotalVictory += 1;
-      } else {
-        player2TotalVictory += 1;
+    const sets = [];
+    for (let i = 0; i <= 2; i++) {
+      let player1SetScore = +player1Scores[i];
+      let player2SetScore = +player2Scores[i];
+      if (player1SetScore && player2SetScore) {
+        player1TotalScore += player1SetScore;
+        player2TotalScore += player2SetScore;
+        if (player1SetScore > player2SetScore) {
+          player1TotalVictory += 1;
+        } else {
+          player2TotalVictory += 1;
+        }
+        sets.push([player1SetScore, player2SetScore]);
+        if (player1TotalVictory === 2 || player2TotalVictory ===2) {
+          break;
+        }
       }
     }
     let winner;
@@ -55,19 +68,25 @@ app.use(bodyParser.json());
     if (!simulate) {
       await db.run(`update player set elo=${winnerNewElo} where player_id=${winner.player_id}`);
       await db.run(`update player set elo=${loserNewElo} where player_id=${loser.player_id}`);
-      const test = await db.run(
+      await db.run(
         `insert into game(winner_id, loser_id) values (${winner.player_id}, ${loser.player_id})`
       );
       const { game_id } = await db.get("select last_insert_rowid() as game_id");
       for (let set of sets) {
+        console.log(set)
         await db.run(
           'insert into set_(game_id, player1_id, player2_id, player1_score, player2_score) values ' +
-          `(${game_id}, ${set.player1Id}, ${set.player2Id}, ${set.player1Score}, ${set.player2Score})`
+          `(${game_id}, ${player1Id}, ${player2Id}, ${set[0]}, ${set[1]})`
         );
       }
     }
-    res.send({winnerNewElo, loserNewElo});
+    res.redirect(303, "/");
   });
+
+  router.get('/', async (req, res) => {
+    const players = await db.all('select * from player order by elo desc');
+    res.render('index', { players });
+  })
 
   //add the router
   app.use('/', router);
